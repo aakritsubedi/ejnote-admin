3eb2891df6b04581f101bb11a639d8b7af88f44b
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-06-10 08:08:19 +0545

    Add Footer

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="footer">
+    <span class="visit-count">This website has been visited: 74519 times.</span>
+    <span class="footer-text">Copyright &copy; 2020. All right reserved. Engineering Juntion.</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style>
+  .footer {
+    background-image: url('../../assets/images/footer.png');
+    height: 100px;
+    padding: 25px 16px;
+  }
+  .footer span {
+    display: block;
+  }
+</style>
