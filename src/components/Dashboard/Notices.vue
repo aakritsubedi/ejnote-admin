eca8ff8ce841a5e3c35a1eea3935ff55f991b1aa
Aakrit Subedi <aakritsubedi@pop-os.localdomain>
2021-04-10 10:15:39 +0545

    Notice and WebApp restructure

---
+++ src/components/Dashboard/Notices.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <h3>
+      <i class="fa fa-bell"></i>
+      Notice
+    </h3>
+    <div class="row">
+      <div class="col-md-8">
+        <ul class="pl-5">
+          <li v-for="(notice, key) in notices" :key="key">
+            <small class="d-block text-muted">{{ notice.source }}</small>
+            <b>{{ notice.title }}</b>
+            <p class="mb-2">
+              {{ notice.description }}
+              <a :href="notice.read_more" class="d-block text-muted text-sm">
+                <small>{{ notice.read_more }}</small>
+              </a>
+            </p>
+          </li>
+        </ul>
+      </div>
+      <div class="col-md-4">
+        <div class="form-group">
+          <label for="title">Title</label>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Enter title"
+            v-model="title"
+          />
+        </div>
+        <div class="form-group">
+          <label for="description">Description</label>
+          <textarea
+            class="form-control"
+            placeholder="Add description"
+            rows="4"
+            v-model="description"
+          />
+        </div>
+        <div class="form-group">
+          <label for="title">Source</label>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Enter source"
+            v-model="source"
+          />
+        </div>
+        <div class="form-group">
+          <label for="title">External Link</label>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Enter link"
+            v-model="link"
+          />
+        </div>
+        <div class="form-group">
+          <button class="btn btn-success" @click="submitNotice">Submit</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Notice",
+  props: ["notices", "addNotice"],
+  data() {
+    return {
+      title: "",
+      description: "",
+      source: "",
+      link: "",
+    };
+  },
+  methods: {
+    submitNotice() {
+      let newNotice = {
+        title: this.title,
+        description: this.description,
+        source: this.source,
+        read_more: this.link,
+      };
+
+      this.addNotice(newNotice);
+      this.title = "";
+      this.description = "";
+      this.source = "";
+      this.link = "";
+    },
+  },
+};
+</script>
--- src/components/Dashboard/WebApp.vue
+++ src/components/Dashboard/WebApp.vue
@@ -2,76 +2,51 @@
   <div>
     <h1>WebAPP</h1>
     <hr />
-    <h3>
-      <i class="fa fa-bell"></i>
-      Notice
-    </h3>
-    <div class="row">
-      <div class="col-md-8">
-        <ul class="pl-5">
-          <li v-for="(notice, key) in notices" :key="key">
-            <b>{{notice.title}}</b>
-            <p>{{notice.description}}</p>
-          </li>
-        </ul>
-      </div>
-      <div class="col-md-4">
-        <div class="form-group">
-          <label for="title">Title</label>
-          <input type="text" class="form-control" placeholder="Enter title" v-model="title" />
-        </div>
-        <div class="form-group">
-          <label for="description">Description</label>
-          <textarea
-            class="form-control"
-            placeholder="Add description"
-            rows="4"
-            v-model="description"
-          />
-        </div>
-        <div class="form-group">
-          <label for="title">Source</label>
-          <input type="text" class="form-control" placeholder="Enter source" v-model="source" />
-        </div>
-        <div class="form-group">
-          <label for="title">External Link</label>
-          <input type="text" class="form-control" placeholder="Enter link" v-model="link" />
-        </div>
-        <div class="form-group">
-          <button class="btn btn-success" @click="addNotice">Submit</button>
-        </div>
-      </div>
-    </div>
+    <Notice :notices="notices" :addNotice="addNotice" />
+    <Alert :alert="alert" v-if="alert.display" />
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from "vuex";
 
+import Alert from "@/components/Alert";
+
+import Notice from "./Notices";
+
 import NoticeService from "@/Services/Notice";
 
 export default {
   name: "WebApp",
+  components: { Notice, Alert },
   data() {
     return {
-      title: '',
-      description: '',
-      source: '',
-      link: ''
-    }
+      alert: {
+        display: false,
+        title: "Hey there",
+        message: "hey there!!!",
+        status: 1,
+        duration: 5000,
+      },
+    };
   },
   methods: {
     ...mapActions(["getNotices"]),
-    addNotice: async function () {
-      let notice = {
-        title: this.title,
-        description: this.description,
-        source: this.source,
-        read_more: this.link
+    addNotice: async function (notice) {
+      try {
+        await NoticeService.addNotice(notice);
+        this.alert = {
+          display: true,
+          title: "Notice added",
+          message: "New notice was added successfully.",
+          status: 1,
+          duration: 5000,
+        };
+        this.getNotices();
+      } catch (e) {
+        console.log("Error sending notice", e);
       }
-
-      await NoticeService.addNotice(notice);
-    }
+    },
   },
   async created() {
     await this.getNotices();
